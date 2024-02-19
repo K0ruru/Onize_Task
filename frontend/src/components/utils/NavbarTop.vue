a2772c0ffd0d0937f97d243a32ca956bce4fa76a
aldiyusronzhar <aldiyusronzhar@gmail.com>
2024-02-19 07:06:45 +0700

    make beautifull

--- frontend/src/components/utils/NavbarTop.vue
+++ frontend/src/components/utils/NavbarTop.vue
@@ -1,78 +1,76 @@
 <script setup>
-	import "primeicons/primeicons.css";
-	import Dropdown from "primevue/dropdown";
-	import { ref, onMounted } from "vue";
 
-	const filter = ref("");
+// primevue
+import "primeicons/primeicons.css";
+import Dropdown from "primevue/dropdown";
+import { ref, onMounted } from "vue";
 
-	// ngambil date
-	const currentDate = ref(new Date());
-	onMounted(() => {
-		// Update the current date every second
-		setInterval(() => {
-			currentDate.value = new Date();
-		}, 1000);
-	});
+const filter = ref("");
 
-	// Define the options for the dropdown
-	const filterOptions = [{ name: "Week" }, { name: "Day" }, { name: "Month" }];
+// ngambil date
+const currentDate = ref(new Date());
+onMounted(() => {
+  // Update the current date every second
+  setInterval(() => {
+    currentDate.value = new Date();
+  }, 1000);
+});
+
+// Define the options for the dropdown
+const filterOptions = [{ name: "Week" }, { name: "Day" }, { name: "Month" }];
 </script>
 
 <template>
-	<!-- navbar-top -->
-	<div class="nav-top">
-		<div class="left-nav">
-			<!-- <p>Jan 2024</p> -->
-			<!-- <p>{{ currentDate.toLocaleString() }}</p> -->
-			<p>
-				{{
-					currentDate.toLocaleString("en-US", {
-						month: "short",
-						day: "2-digit",
-						year: "numeric",
-					})
-				}}
-			</p>
-			<Dropdown
-				v-model="filter"
-				:options="filterOptions"
-				optionLabel="name"
-				placeholder="Week"
-				class="w-full md:w-14rem"
-			/>
+  <!-- navbar-top -->
+  <div class="nav-top">
+    <div class="left-nav">
+      <!-- <p>Jan 2024</p> -->
+      <!-- <p>{{ currentDate.toLocaleString() }}</p> -->
+      <p>
+        {{
+          currentDate.toLocaleString("en-US", {
+            month: "short",
+            day: "2-digit",
+            year: "numeric",
+          })
+        }}
+      </p>
+      <Dropdown v-model="filter" :options="filterOptions" optionLabel="name" placeholder="Week"
+        class="w-full md:w-14rem" />
 
-			<div class="filter"><i class="pi pi-tag"></i>Filter</div>
-		</div>
-		<div class="right-nav">
-			<i class="pi pi-circle-fill"></i>
-		</div>
-	</div>
+      <div class="filter"><i class="pi pi-tag"></i>Filter</div>
+    </div>
+    <div class="right-nav">
+      <i class="pi pi-circle-fill"></i>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-	.filter {
-		display: flex;
-		align-items: center;
-		gap: 7px;
-		cursor: pointer;
-	}
-	.nav-top {
-		display: flex;
-		justify-content: space-between;
-		width: 100%;
-		border-bottom: 1px solid #222;
-		padding: 1.7%;
-		align-items: center;
-	}
+.filter {
+  display: flex;
+  align-items: center;
+  gap: 7px;
+  cursor: pointer;
+}
 
-	.left-nav {
-		display: flex;
-		align-items: center;
-		gap: 10px;
-	}
+.nav-top {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  border-bottom: 1px solid #222;
+  padding: 1.7%;
+  align-items: center;
+}
 
-	.right-nav {
-		color: #6427aa;
-		padding-right: 15px;
-	}
+.left-nav {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.right-nav {
+  color: #6427aa;
+  padding-right: 15px;
+}
 </style>
